da6094966ada9575c5cd3a2a2a54a1c258ded5ba
Tch0p <103029128+Tch0p@users.noreply.github.com>
2022-04-08 14:21:31 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,36 @@
-basic.forever(function () {
-    if (input.lightLevel() <= 100) {
-        if (true) {
-        	
-        } else {
-        	
-        }
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    pins.analogWritePin(AnalogPin.P0, 338)
+    radio.sendString("test")
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
     }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    pins.analogWritePin(AnalogPin.P0, 0)
+})
+radio.setGroup(0)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
